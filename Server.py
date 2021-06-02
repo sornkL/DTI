fb7f1049deb439136d3d3aee0e4062568f77a251
Sornk <598839297@qq.com>
2021-06-02 23:27:44 +0800

    Server.py

--- Server.py
+++ Server.py
@@ -1,14 +1,34 @@
-import socket
+import socketserver
+import struct
+
+from typing import Tuple
 
 from Settings import *
 
-server = socket.socket()
-host = socket.gethostname()
+TCP_LENGTH = 4
 
-server.bind((host, PORT))
+class TCPHandler(socketserver.BaseRequestHandler):
+    def handle(self) -> None:
+        while True:
+            dataLengthStr = self.request.recv(4)
+            dataLength = sum(list(bytes(dataLengthStr)))
+            self.data = self.request.recv(dataLength).decode('UTF-8', 'ignore').strip()
+            if not self.data:
+                break
+            print(TCPHandler.get_ligand_target(self.data))
+            self.feedback_data = "Return message".encode('utf-8')
+            self.request.sendall(self.feedback_data)
 
-server.listen(5)
-while True:
-    c, addr = server.accept()
-    c.send('Hello, world'.encode())
-    c.close()
+    @staticmethod
+    def get_ligand_target(pattern: str) -> Tuple[str, str, str]:
+        uid, ligandSmiles, targetFASTA = pattern.split('&')
+
+        return uid, ligandSmiles, targetFASTA
+
+
+if __name__ == '__main__':
+    host = LOCALHOST
+    port = PORT
+    server = socketserver.ThreadingTCPServer((host, port), TCPHandler)
+    server.serve_forever()
+    server.server_close()
